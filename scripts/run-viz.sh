d13d6eb325428aeeccab5937e6ff3eca6f453d03
clarabakker <57916928+clarabakker@users.noreply.github.com>
2021-03-17 09:44:39 -0400

    Tool path fix

--- scripts/run-viz.sh
+++ scripts/run-viz.sh
@@ -21,7 +21,7 @@ base=${bedpe%.gz}
 
 # call gaussian smoother
 python3 /usr/local/bin/call_gauss.py --in_bedpe ${base} --chr_sizes $chr_sizes --outname $outdir/$out.bedgraph --base_direc $base_direc
-./bedGraphToBigWig $outdir/$out.bedgraph $chr_sizes $outdir/$out.bw
+/usr/local/bin/bedGraphToBigWig $outdir/$out.bedgraph $chr_sizes $outdir/$out.bw
 
 
 rm ${bedpe%.gz}
