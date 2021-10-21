22013bc7d987046c8bb82cfc8f9fa57fbd2138eb
clarabakker <57916928+clarabakker@users.noreply.github.com>
2021-10-21 08:50:17 -0400

    Fix extraneous conditional

--- scripts/run-viz.sh
+++ scripts/run-viz.sh
@@ -22,10 +22,7 @@ base=${bedpe%.gz}
 # call gaussian smoother
 python3 /usr/local/bin/call_gauss.py --in_bedpe ${base} --chr_sizes $chr_sizes --outname $outdir/$out.bedgraph --base_direc $base_direc
 
-if ! $is_ctl
-then
-    /usr/local/bin/bedGraphToBigWig $outdir/$out.bedgraph $chr_sizes $outdir/$out.bw
-fi
+/usr/local/bin/bedGraphToBigWig $outdir/$out.bedgraph $chr_sizes $outdir/$out.bw
 
 gzip -f $outdir/$out.bedgraph
 
